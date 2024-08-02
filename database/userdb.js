0da15c6160c828ea57742ef26edd174dbff4b74b
UserDevAkku <aakashverma.1234.abcd@gmail.com>
2024-08-02 07:09:56 +0530

    update userdb.js

--- database/userdb.js
+++ database/userdb.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const connectiondb=mongoose.connect('mongodb://localhost:27017/dbs').then(()=>{
+const connectiondb=mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('connection establshed');
 }).catch((error)=>{
     console.log('connection is not established');
